754bb8a7883d91aae451a08a050db215e6785c60
Jungminchae <minchae3618@gmail.com>
2021-03-23 16:34:00 +0900

    add user serializer and in room serializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from users.serializers import TinyUserSerializer
 from .models import Room
 
 
@@ -6,6 +7,8 @@ class RoomSerializer(serializers.ModelSerializer):
     """
     Serializer => python 객체에서 json 객체로 바꿔 주는 것
     """
+
+    user = TinyUserSerializer()
 
     class Meta:
         model = Room
