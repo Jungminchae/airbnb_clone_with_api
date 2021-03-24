252698fc607103bcc571596720e34673d4a03ef8
Jungminchae <minchae3618@gmail.com>
2021-03-24 19:01:34 +0900

    add ListView and Default Pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,10 @@
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 
 
-class ListRoomsView(APIView):
-    def get(self, request):
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
+class ListRoomsView(ListAPIView):
+    # 커스터 마이징 할 것이 없을 때 generic view를 쓰면 됨
+    queryset = Room.objects.all()  # query set 만들고
+    serializer_class = RoomSerializer  # serializer 만들면 끝
