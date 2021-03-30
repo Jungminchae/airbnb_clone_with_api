e23399b19698d225959574c8091e5ecd7f21e914
Jungminchae <minchae3618@gmail.com>
2021-03-30 15:35:46 +0900

    add seeroomview

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,15 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, BigRoomSerializer
 
 
 class ListRoomsView(ListAPIView):
     # 커스터 마이징 할 것이 없을 때 generic view를 쓰면 됨
     queryset = Room.objects.all()  # query set 만들고
     serializer_class = RoomSerializer  # serializer 만들면 끝
+
+
+class SeeRoomView(RetrieveAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
