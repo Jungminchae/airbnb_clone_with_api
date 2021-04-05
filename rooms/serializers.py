e65fd629badf976465e9ff167aafca4118543364
Jungminchae <minchae3618@gmail.com>
2021-04-05 23:15:53 +0900

    back to views and delete viewsets for now

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from users.serializers import TinyUserSerializer
+from users.serializers import UserSerializer
 from .models import Room
 
 
@@ -8,14 +8,8 @@ class RoomSerializer(serializers.ModelSerializer):
     Serializer => python 객체에서 json 객체로 바꿔 주는 것
     """
 
-    user = TinyUserSerializer()
+    user = UserSerializer()
 
     class Meta:
         model = Room
-        fields = ("pk", "name", "price", "instant_book", "user")
-
-
-class BigRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        exclude = ()
+        fields = ("pk", "name", "price", "user")
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import viewsets
+from .views import ListRoomsView, SeeRoomView
 
 
 app_name = "rooms"
 
-router = DefaultRouter()
-router.register("", viewsets.RoomViewset, basename="room")
 
-
-urlpatterns = router.urls
+urlpatterns = [
+    path("list/", ListRoomsView.as_view()),
+    path("<int:pk>/", SeeRoomView.as_view()),
+]
