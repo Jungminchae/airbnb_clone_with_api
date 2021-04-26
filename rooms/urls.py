73b6abd4cf40ecd6105b61befaed074d2c3bd06b
Jungminchae <minchae3618@gmail.com>
2021-04-26 23:15:15 +0900

    Room View with APIView : class based view

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from .views import SeeRoomView, rooms_view
+from .views import SeeRoomView, RoomView
 
 
 app_name = "rooms"
 
 
 urlpatterns = [
-    path("", rooms_view),
+    path("", RoomView.as_view()),
     path("<int:pk>/", SeeRoomView.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,13 +8,32 @@ from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
 
 # django view가 아니라 api가 작동하게 하기 위해서 필요
-@api_view(["GET", "POST"])
-def rooms_view(request):
-    if request.method == "GET":
+# @api_view(["GET", "POST"])
+# def rooms_view(request):
+#     if request.method == "GET":
+#         rooms = Room.objects.all()
+#         serializer = ReadRoomSerializer(rooms, many=True).data
+#         return Response(serializer)
+#     elif request.method == "POST":
+#         # POST => Create
+#         if not request.user.is_authenticated:
+#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+#         serializer = WriteRoomSerializer(data=request.data)
+#         if serializer.is_valid():
+#             room = serializer.save(user=request.user)
+#             room_serializer = ReadRoomSerializer(room).data
+#             return Response(data=room_serializer, status=status.HTTP_200_OK)
+#         else:
+#             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomView(APIView):
+    def get(self, request):
         rooms = Room.objects.all()
         serializer = ReadRoomSerializer(rooms, many=True).data
         return Response(serializer)
-    elif request.method == "POST":
+
+    def post(self, request):
         # POST => Create
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
